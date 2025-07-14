5ef4bd275e07706a7c4894ee1633ba7985d4f99d
Baastheglass <lightningblazer123@gmail.com>
2025-07-14 21:00:57 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from pathlib import Path
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Created: {event.src_path}")
+        print("Using pathlib:", Path(event.src_path).name)
+    def on_modified(self, event):
+        pass
+        #print(f"Modified: {event.src_path}")
+    def on_deleted(self, event):
+        pass
+        #print(f"Deleted: {event.src_path}")
+    def on_moved(self, event):
+        pass
+        #print(f"Moved: {event.src_path} -> {event.dest_path}")
+
+if __name__ == "__main__":
+    path = "C:\\Users\\Well\\Desktop\\VSCODE\\config-file-updater"
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    print(f"Watching: {path}")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
