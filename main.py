f991f95427c4a0997c58fac141912fb53f745fdc
Baastheglass <lightningblazer123@gmail.com>
2025-07-14 22:11:57 +0500

    changing logic for switching directory

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class MyHandler(FileSystemEventHandler):
         folder_name = Path(event.src_path).name
         if(".pid" not in folder_name):
             print("Created:", folder_name)
-            os.chdir(os.path.dirname(event.src_path) + '/' + folder_name)
+            os.chdir('root/CI_Server')
             print(os.getcwd())
     def on_modified(self, event):
         pass
